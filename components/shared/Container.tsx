9286b4ce58f26c1256394230122cb7deef90b0e2
Yura Kasyan <33.yurakasyan@gmail.com>
2024-12-08 16:02:38 +0100

    feat: adds shared Container.tsx component

---
+++ components/shared/Container.tsx
@@ -0,0 +1,14 @@
+import clsx from 'clsx';
+
+interface ContainerProps {
+  className?: string;
+  children: React.ReactNode;
+}
+
+const Container = ({ className, children }: ContainerProps) => {
+  return (
+    <div className={clsx('px-4 mx-auto max-w-7xl', className)}>{children}</div>
+  );
+};
+
+export default Container;
