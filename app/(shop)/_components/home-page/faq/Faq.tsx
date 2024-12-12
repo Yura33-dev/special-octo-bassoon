1c27dc7ebe4f0766171dda1ab5a13232402ecf11
Yura Kasyan <33.yurakasyan@gmail.com>
2024-12-12 21:50:11 +0100

    feat: adds FAQ section to main page

---
+++ app/(shop)/_components/home-page/faq/Faq.tsx
@@ -0,0 +1,53 @@
+export default function Faq() {
+  return (
+    <div className='mt-8 sm:max-w-[70%] mx-auto'>
+      <div className='collapse collapse-arrow bg-white mt-4 '>
+        <input type='radio' name='my-accordion-2' />
+        <h3 className='collapse-title text-xl font-medium'>
+          Чи гарантована якість ваших насінь та добрив?
+        </h3>
+        <div className='collapse-content'>
+          <p>
+            Так, ми приймаємо повернення або обмін товару відповідно до наших
+            умов повернення. Якщо ви не задоволені отриманим товаром, будь
+            ласка, зв`яжіться з нашою службою підтримки клієнтів протягом 7 днів
+            після отримання замовлення для отримання додаткової інформації та
+            інструкцій щодо повернення або обміну.
+          </p>
+        </div>
+      </div>
+
+      <div className='collapse collapse-arrow bg-white mt-4 '>
+        <input type='radio' name='my-accordion-2' />
+        <h3 className='collapse-title text-xl font-medium'>
+          Чи є можливість повернення або обміну товару?
+        </h3>
+        <div className='collapse-content'>
+          <p>
+            Так, ми приймаємо повернення або обмін товару відповідно до наших
+            умов повернення. Якщо ви не задоволені отриманим товаром, будь
+            ласка, зв`яжіться з нашою службою підтримки клієнтів протягом 7 днів
+            після отримання замовлення для отримання додаткової інформації та
+            інструкцій щодо повернення або обміну.
+          </p>
+        </div>
+      </div>
+
+      <div className='collapse collapse-arrow bg-white mt-4 '>
+        <input type='radio' name='my-accordion-2' />
+        <h3 className='collapse-title text-xl font-medium'>
+          Чи можна відстежити моє замовлення?
+        </h3>
+        <div className='collapse-content'>
+          <p>
+            Так, ми приймаємо повернення або обмін товару відповідно до наших
+            умов повернення. Якщо ви не задоволені отриманим товаром, будь
+            ласка, зв`яжіться з нашою службою підтримки клієнтів протягом 7 днів
+            після отримання замовлення для отримання додаткової інформації та
+            інструкцій щодо повернення або обміну.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
