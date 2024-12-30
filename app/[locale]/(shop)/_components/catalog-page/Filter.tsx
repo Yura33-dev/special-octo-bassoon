8d4b91830beca0479ee128d8f4e9d88870af4a41
Yura Kasyan <33.yurakasyan@gmail.com>
2024-12-30 17:23:24 +0100

    chore: adds template for Filter component

---
+++ app/[locale]/(shop)/_components/catalog-page/Filter.tsx
@@ -0,0 +1,9 @@
+// interface IFilterProps {}
+
+export default function Filter() {
+  return (
+    <aside className='basis-full lg:basis-[310px] flex-shrink-0 bg-red-400'>
+      Filter
+    </aside>
+  );
+}
