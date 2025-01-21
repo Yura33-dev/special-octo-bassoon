279dc8ad0b2c6a2527fc4b39668783d5f7c13667
Yura Kasyan <33.yurakasyan@gmail.com>
2025-01-21 17:41:20 +0100

    refactor: adds dynamic categories and translations to Footer

--- app/[locale]/(shop)/_components/shared/main-footer/Footer.tsx
+++ app/[locale]/(shop)/_components/shared/main-footer/Footer.tsx
@@ -1,35 +1,48 @@
 import { Clock, Mail, MapPin, Phone } from 'lucide-react';
-import Link from 'next/link';
+import { getLocale, getTranslations } from 'next-intl/server';
 
 import Container from '@/components/shared/Container';
+import { Link } from '@/i18n/routing';
+import { getAllCategories, getAllSettings } from '@/lib/api';
+import { locale } from '@/types';
 
-import data from './categoriesData.json';
 import Logo from '../../ui/Logo';
 
-export default function Footer() {
+export default async function Footer() {
+  const locale = (await getLocale()) as locale;
+
+  const [t, settings, categories] = await Promise.all([
+    getTranslations('Footer'),
+    getAllSettings(locale),
+    getAllCategories(locale, {
+      visible: true,
+      main: true,
+    }),
+  ]);
+
   return (
     <footer className='mt-20 bg-primary text-white py-8'>
       <Container>
         <div className='flex flex-col items-center mb-6 lg:mb-12'>
           <Logo className='text-2xl py-2' />
-          <span className='text-sm'>Засій своє майбутнє з ProGround!</span>
+          <span className='text-sm'>{settings[0].translatedData.slogan}</span>
         </div>
 
         <div className='flex flex-col gap-8 sm:gap-0 sm:gap-x-4 sm:gap-y-6 sm:flex-row sm:flex-wrap sm:justify-between'>
           <ul className='flex flex-col items-center sm:items-start '>
             <li className='mb-3'>
-              <h3 className='text-xl font-semibold'>Категорії</h3>
+              <h3 className='text-xl font-semibold'>{t('CategoriesTitle')}</h3>
             </li>
-            {data.categoriesData.map((category, index) => (
-              <li key={index} className='w-max relative'>
+            {categories.map(category => (
+              <li key={category.id} className='w-max relative'>
                 <Link
-                  href={category.href}
+                  href={`/catalog/${category.slug}`}
                   className='flex p-1  
                           after:content-[""] after:block after:w-full after:h-[2px] after:bg-accent after:absolute
                           after:bottom-0 after:left-1/2 after:-translate-x-1/2 after:max-w-0 after:transition-all after:duration-150
                           hover:after:max-w-[calc(100%_-_8px)]'
                 >
-                  {category.text}
+                  {category.name}
                 </Link>
               </li>
             ))}
@@ -37,7 +50,7 @@ export default function Footer() {
 
           <ul className='flex flex-col items-center sm:items-start '>
             <li className='mb-3'>
-              <h3 className='text-xl font-semibold'>Контакти</h3>
+              <h3 className='text-xl font-semibold'>{t('ContactsTitle')}</h3>
             </li>
             <li className='p-1 flex gap-2 justify-start items-center relative w-max'>
               <Phone className='w-4 h-4' />
@@ -87,7 +100,7 @@ export default function Footer() {
 
           <ul className='flex flex-col items-center sm:items-start '>
             <li className='mb-3'>
-              <h3 className='text-xl font-semibold'>Покупцям</h3>
+              <h3 className='text-xl font-semibold'>{t('CustomersTitle')}</h3>
             </li>
             <li className='p-1 flex gap-2 justify-start items-center relative w-max'>
               <Link
@@ -133,7 +146,7 @@ export default function Footer() {
 
           <ul className='flex flex-col items-center sm:items-start '>
             <li className='mb-3'>
-              <h3 className='text-xl font-semibold'>Графік роботи</h3>
+              <h3 className='text-xl font-semibold'>{t('WorkSchedule')}</h3>
             </li>
             <li className='p-1 flex gap-2 justify-start items-center'>
               <Clock className='w-4 h-4' />
