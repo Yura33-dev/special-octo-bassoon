6e99081bcfbea94a49983a9daca8b6a74560a47e
Yura Kasyan <33.yurakasyan@gmail.com>
2025-05-09 23:29:36 +0200

    chore: edits docker-compose and project config file

--- lib/config/index.ts
+++ lib/config/index.ts
@@ -4,6 +4,6 @@ export const config = {
     process.env.NEXT_PUBLIC_APP_URL || 'http://127.0.0.1:3000',
   s3_bucket_name: process.env.S3_BUCKET_NAME || '',
   s3_region: process.env.S3_REGION || '',
-  aws_access_key: process.env.AWS_ACCESS_KEY || '',
-  aws_secret_access_key: process.env.AWS_SECRET_ACCESS_KEY || '',
+  aws_access_key: process.env.S3_ACCESS_KEY || '',
+  aws_secret_access_key: process.env.S3_SECRET_ACCESS_KEY || '',
 };
