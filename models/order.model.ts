2fc66dd5eb88eb35fac87b47b37e28202c8dee71
Yura Kasyan <33.yurakasyan@gmail.com>
2025-06-12 18:44:59 +0200

    fix: add last one type of order status

--- models/order.model.ts
+++ models/order.model.ts
@@ -29,7 +29,7 @@ const orderSchema = new mongoose.Schema<IOrderApi>(
     fatherName: { type: String, default: null },
     status: {
       type: String,
-      enum: ['new', 'processing', 'delivery', 'done'],
+      enum: ['new', 'processing', 'delivery', 'done', 'canceled'],
       default: 'new',
       required: true,
     },
