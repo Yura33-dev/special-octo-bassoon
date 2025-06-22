4f9d59fe190a97f8f53183f4cfda1844305c66db
Yura Kasyan <33.yurakasyan@gmail.com>
2025-06-22 21:29:07 +0200

    refactor: updates seo for main page

--- app/[locale]/(shop)/page.tsx
+++ app/[locale]/(shop)/page.tsx
@@ -2,69 +2,79 @@ import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
 import { getPageDataByName } from '@/lib/api';
+import { config } from '@/lib/config';
 import { locale } from '@/types';
 
 import HomePage from './_components/home-page/HomePage';
 
-// TODO: add SEO images in OG
 export async function generateMetadata({
   params,
 }: IShopHomeProps): Promise<Metadata> {
   const locale = params?.locale ?? 'uk'; // 🔒 fallback
 
   const data = await getPageDataByName('MainPage');
-  const translation = data?.translatedData?.[locale];
 
   if (!data)
     return {
       title: 'Насіння оптом та в роздріб з доставкою по всій Україні',
       description:
-        'Купити насіння з доставкою по Україні. Інтернет магазин продажу насіння.✔️Гарантія якості ✔️Вигідні ціни ✔️Швидка доставка',
+        'Купити насіння з доставкою по Україні. Інтернет магазин продажу насіння ProGround.✔️Гарантія якості ✔️Вигідні ціни ✔️Швидка доставка',
     };
+
+  const translation = data.translatedData[locale];
+
+  const currentUrl = `${config.NEXT_PUBLIC_APP_URL}/${locale}`;
 
   return {
     title:
-      translation?.meta.title ??
+      translation.meta.title ??
       'Насіння оптом та в роздріб з доставкою по всій Україні',
-    description: translation?.meta.description ?? '',
-    keywords: translation?.meta.keywords ?? '',
+    metadataBase: new URL(config.NEXT_PUBLIC_APP_URL),
 
-    openGraph: {
+    alternates: {
+      canonical: currentUrl,
+      languages: {
+        uk: `${config.NEXT_PUBLIC_APP_URL}/uk}`,
+        ru: `${config.NEXT_PUBLIC_APP_URL}/ru}`,
+        'x-default': `${config.NEXT_PUBLIC_APP_URL}/uk}`,
+      },
+    },
+
+    other: {
       title:
         translation?.meta.title ??
         'Насіння оптом та в роздріб з доставкою по всій Україні',
-      description: translation?.meta.description ?? '',
+      description:
+        translation?.meta.description ??
+        'Купити насіння з доставкою по Україні. Інтернет магазин продажу насіння ProGround.✔️Гарантія якості ✔️Вигідні ціни ✔️Швидка доставка',
+      keywords:
+        translation?.meta.keywords ??
+        'насіння, засоби захисту рослин, ProGround, інтернет-магазин насіння',
+    },
+
+    openGraph: {
+      title: translation.meta.title,
+      description: translation.meta.description,
+      url: `/${locale}`,
+      type: 'website',
+      images: [
+        {
+          url: translation.meta.image,
+          width: 1200,
+          height: 630,
+          alt: translation.meta.title,
+        },
+      ],
+    },
+
+    twitter: {
+      card: 'summary_large_image',
+      title: translation.meta.title,
+      description: translation.meta.description,
+      images: [translation.meta.image],
     },
   };
 }
-
-// export async function generateMetadata({
-//   params,
-// }: IShopHomeProps): Promise<Metadata> {
-//   const data = await getPageDataByName('MainPage');
-
-// if (!data)
-//   return {
-//     title: 'Насіння оптом та в роздріб з доставкою по всій Україні',
-//     description:
-//       'Купити насіння з доставкою по Україні. Інтернет магазин продажу насіння.✔️Гарантія якості ✔️Вигідні ціни ✔️Швидка доставка',
-//   };
-
-// return {
-//   title:
-//     data.translatedData[params.locale].meta.title ??
-//     'Насіння оптом та в роздріб з доставкою по всій Україні',
-//   description: data.translatedData[params.locale].meta.description ?? '',
-//   keywords: data.translatedData[params.locale].meta.keywords ?? '',
-
-//   openGraph: {
-//     title:
-//       data.translatedData[params.locale].meta.title ??
-//       'Насіння оптом та в роздріб з доставкою по всій Україні',
-//     description: data.translatedData[params.locale].meta.description ?? '',
-//   },
-// };
-// }
 
 interface IShopHomeProps {
   params: {
