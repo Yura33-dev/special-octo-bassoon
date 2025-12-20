14db2c630775da73856c9c8a0fe60dac0b54792a
Yura Kasyan <33.yurakasyan@gmail.com>
2025-12-20 19:35:59 +0100

    fix: adds seo-title for tab in checkout page

--- app/[locale]/(shop)/checkout/page.tsx
+++ app/[locale]/(shop)/checkout/page.tsx
@@ -1,9 +1,39 @@
+import { Metadata } from 'next';
 import { getTranslations } from 'next-intl/server';
 
 import Container from '@/components/shared/Container';
+import { config } from '@/lib/config';
+import { locale } from '@/types';
 
 import CheckoutCart from '../_components/checkout-page/checkout-cart/CheckoutCart';
 import CheckoutForm from '../_components/checkout-page/checkout-form/CheckoutForm';
+
+interface ICheckoutPageProps {
+  params: {
+    locale: locale;
+  };
+}
+
+export async function generateMetadata({
+  params,
+}: ICheckoutPageProps): Promise<Metadata> {
+  const t = await getTranslations('CheckoutPage');
+
+  const currentUrl = `${config.NEXT_PUBLIC_APP_URL}/${params.locale}/checkout`;
+
+  return {
+    title: t('CheckoutTitle'),
+    metadataBase: new URL(config.NEXT_PUBLIC_APP_URL),
+    alternates: {
+      canonical: currentUrl,
+      languages: {
+        uk: `${config.NEXT_PUBLIC_APP_URL}/uk/checkout`,
+        ru: `${config.NEXT_PUBLIC_APP_URL}/ru/checkout`,
+        'x-default': `${config.NEXT_PUBLIC_APP_URL}/uk/checkout`,
+      },
+    },
+  };
+}
 
 export default async function CheckoutPage() {
   const t = await getTranslations('CheckoutPage');
