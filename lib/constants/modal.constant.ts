c30737091f0afb3d463e08316d3d7b73bcd4a149
Yura Kasyan <33.yurakasyan@gmail.com>
2025-12-23 16:23:13 +0100

    refactor: get rid of previous changes in ModalWindow

--- lib/constants/modal.constant.ts
+++ lib/constants/modal.constant.ts
@@ -1,8 +1,5 @@
 // MODALS IDs
 
-// Shop
-export const SUCCESS_ORDER_ID = 'success_order_id';
-
 // Dashboard
 export const ADD_CATEGORY_ID = 'add_category_id';
 export const DELETE_CATEGORY_ID = 'delete_category_id';
