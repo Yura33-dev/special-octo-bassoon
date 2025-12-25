a1f4d14af9e52cd5419e8ae5151015497f05b868
Yura Kasyan <33.yurakasyan@gmail.com>
2025-12-25 13:34:11 +0100

    refactor: changes in ClearCartOnSuccess component

--- app/[locale]/(shop)/_components/checkout-page/success-page/ClearCartOnSuccess.tsx
+++ app/[locale]/(shop)/_components/checkout-page/success-page/ClearCartOnSuccess.tsx
@@ -6,14 +6,26 @@ import { useCartStore } from '@/providers';
 
 export default function ClearCartOnSuccess({ order }: { order?: string }) {
   const cleanCart = useCartStore(state => state.cleanCart);
+  const total = useCartStore(state => state.getTotalPrice());
 
   useEffect(() => {
     if (!order) return;
+
     const key = `order-cleared-${order}`;
+
     if (sessionStorage.getItem(key)) return;
+
+    if (window.dataLayer && total) {
+      window.dataLayer.push({
+        event: 'successful_order',
+        orderId: order,
+        total: total / 100,
+      });
+    }
+
     cleanCart();
     sessionStorage.setItem(key, '1');
-  }, [order, cleanCart]);
+  }, [order, cleanCart, total]);
 
   return null;
 }
