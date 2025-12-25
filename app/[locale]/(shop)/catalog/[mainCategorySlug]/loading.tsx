c7698fa4481f6e08caba602a2df45d3aeb677ee3
Yura Kasyan <33.yurakasyan@gmail.com>
2025-12-25 20:39:09 +0100

    chore: deletes unnecessary loadings

--- app/[locale]/(shop)/catalog/[mainCategorySlug]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import CircleLoader from '@/components/shared/loaders/CircleLoader';
-
-export default function Loading() {
-  return (
-    <div className='h-[250px] flex justify-center items-center'>
-      <CircleLoader className='w-14 h-14' />
-    </div>
-  );
-}
