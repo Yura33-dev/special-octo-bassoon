6ad242629fb54b99250233071701a4b77e3972ac
Yura Kasyan <33.yurakasyan@gmail.com>
2025-12-28 13:48:10 +0100

    refactor: enhance CardLabelDiscount component to accept custom className for styling

--- app/[locale]/(shop)/_components/shared/catalogGrid/product-card/CardLabelDiscount.tsx
+++ app/[locale]/(shop)/_components/shared/catalogGrid/product-card/CardLabelDiscount.tsx
@@ -1,18 +1,27 @@
 'use client';
 
+import clsx from 'clsx';
+
 import { formattedDiscount } from '@/lib/utils';
 
 interface ICardLabelDiscountProps {
   oldPrice: number;
   price: number;
+  className?: string;
 }
 
 export default function CardLabelDiscount({
   oldPrice,
   price,
+  className,
 }: ICardLabelDiscountProps) {
   return (
-    <span className='block bg-red-600 text-white text-xs rounded-md w-max px-1.5 py-0.5'>
+    <span
+      className={clsx(
+        'block bg-red-600 text-white text-xs rounded-md w-max px-1.5 py-0.5',
+        className && className
+      )}
+    >
       -{formattedDiscount(oldPrice, price)}
     </span>
   );
